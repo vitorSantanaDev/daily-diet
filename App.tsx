89af9917fc665947fb751cad04f30e7f35f7c933
vitorSantanaDev <vitorsantana.developer@gmail.com>
2023-12-11 07:06:15 -0300

    chore: save

--- App.tsx
+++ App.tsx
@@ -3,25 +3,28 @@ import { StatusBar, ActivityIndicator } from "react-native";
 import { ThemeProvider } from "styled-components";
 
 import {
-  useFonts,
-  NunitoSans_700Bold,
-  NunitoSans_400Regular,
+	useFonts,
+	NunitoSans_700Bold,
+	NunitoSans_400Regular,
 } from "@expo-google-fonts/nunito-sans";
 
 import { Routes } from "@routes/index";
 import theme from "@theme/index";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
-  const [fontsLoaded] = useFonts({ NunitoSans_400Regular, NunitoSans_700Bold });
+	const [fontsLoaded] = useFonts({ NunitoSans_400Regular, NunitoSans_700Bold });
 
-  return (
-    <ThemeProvider theme={theme}>
-      <StatusBar
-        translucent
-        barStyle="light-content"
-        backgroundColor="transparent"
-      />
-      {fontsLoaded ? <Routes /> : <ActivityIndicator />}
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider theme={theme}>
+			<StatusBar
+				translucent
+				barStyle="dark-content"
+				backgroundColor="transparent"
+			/>
+			<SafeAreaProvider>
+				{fontsLoaded ? <Routes /> : <ActivityIndicator />}
+			</SafeAreaProvider>
+		</ThemeProvider>
+	);
 }
